9f86dcb5bc4c84dcc35afdb155214e3ad47a1d12
stevenrhett <johnsonsteven18@gmail.comclear>
2022-08-28 16:18:37 -0400

    this is another commit

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-const townCity = [
-  'Weymouth',
-  'Quincy',
-  'Braintree',
-  'Hingham',
-  'Brockton'
+const names = [
+  "Weymouth",
+  "Quincy",
+  "Brockton",
+  "Braintree",
+  "Stoughton"
 ];
+
+const inputField = document.getElementById("searchField");
+const resultWrapper = document.querySelector("#resultWrapper");
+const result = document.getElementById("results");
+const cleanBtn = document.getElementById("closebtn");
+
+cleanBtn.addEventListener('click', function () {
+  inputField.value = "";
+  resultWrapper.classList.add('hidden');
+  console.log('clear button')
+});
+
+inputField.addEventListener('input', function () {
+  let keyInput = inputField.value;
+  console.log(keyInput);
+  for (let item of names) {
+
+    if (item.toLocaleLowerCase() === keyInput) {
+      resultWrapper.classList.remove('hidden');
+      result.innerHTML = `<li>${item}</li>`;
+      console.log(`${item}`);
+    }
+    if (!keyInput.toLocaleLowerCase()) {
+      resultWrapper.classList.add('hidden');
+    }
+
+  }
+});
+
